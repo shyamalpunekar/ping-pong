24ec2edd3a8b9fe2680e6d459174e24ffca28518
shy <shyamal.punekar@gmail.com>
2017-07-14 11:48:18 -0700

    add jquery for retrieving input from user

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,15 @@ $(document).ready(function() {
 
   $("#gameplay").submit(function(event) {
     event.preventDefault();
+    alert("hello");
+    var inputNumber = parseInt($("#inputNumber").val());
+    var outputNumber = pingPong(inputNumber);
+      for(index=0; index<inputNumber; inputNumber++){
+        $("#displayOutput").append("<li>" + outputNumber[i] + "</li>");
+      }
+  });
 
-
+  $("#about").click(function() {
+    $(".about").show();
   });
 });
-
-var items = ["item1", "item2", "item3", "item4", "item5"]
-var itemVals = items.map(function(item) {
-  return $("#"  + item).val().toUpperCase();
-});
-itemVals.sort();
-itemVals.forEach(function(itemVal){
-  $(".checkout-items").append("<li>" + itemVal + "</li>");
-});
-$("#grocery-list").show();
-$("#grocery-list").show();
