b6ea01c8928f0b1d4a9388cdbc3b990358d5d0ce
shy <shyamal.punekar@gmail.com>
2017-07-14 12:14:43 -0700

    Add function with different use cases like divisible by 3, divisible 5 divisible by 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
+
+// business interface
+var pingPong = function(inputNumber) {
+var playOutcome = pingPong.map(function(inputNumber) {
+  for(index = 0; index < inputNumber; index++){
+    if(index % 3 === 0 && index % 15 !== 0){
+      playOutcome.push("Ping");
+    } else if (index % 5 === 0 && index % 15 !== 0) {
+      playOutcome.push("Pong");
+    } else if (index % 15 === 0) {
+      playOutcome.push("Ping-Pong");
+    } else {
+      playOutcome.push(inputNumber);
+    }
+    }
+    return inputNumber;
+})
+}
+
+// User interface
 $(document).ready(function() {
-
-
   $("#gameplay").submit(function(event) {
     event.preventDefault();
-    alert("hello");
     var inputNumber = parseInt($("#inputNumber").val());
     var outputNumber = pingPong(inputNumber);
       for(index=0; index<inputNumber; inputNumber++){
